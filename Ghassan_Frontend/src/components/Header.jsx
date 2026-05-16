d61cfa10ecc3cdd27ce1d019207ebe742678314a
Ibrahim <ebrahimshabebi@gmail.com>
2026-05-16 18:19:54 +0300

    Header added

---
+++ Ghassan_Frontend/src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Header.css";
+import logo from "../assets/logo.png";
+
+export default function Header() {
+  return (
+    <header className="header">
+      {/* Navigation */}
+      <nav className="nav-links">
+        <a href="#">منتجاتنا</a>
+        <a href="#">من نحن</a>
+        <a href="#">الرئيسية</a>
+      </nav>
+
+      {/* Logo + Text */}
+      <div className="brand">
+        <div className="brand-text">
+          <h1>مؤسسة غسان البطاطي للتجارة</h1>
+          <p>Ghassan Al-Batati Trading Establishment</p>
+        </div>
+
+        <img
+          src={logo}
+          alt="Ghassan Logo"
+          className="logo"
+        />
+      </div>
+    </header>
+  );
+}
